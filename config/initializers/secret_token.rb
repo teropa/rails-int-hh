f426c9cc00a2366958f9153566433d7631477b96
Tair Assimov <tair@assimov.net>
2011-11-15 12:01:22 +0200

    Setting up:
    * Install Bundler - Application dependency manager
    gem install bundler
    * Install Rails
    gem install rails
    * Create a new Rails application
    rails new library
    * Start the server
    rails server
    * Hello, Rails! Open the browser at
    http://localhost:3000

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = 'abd2b2c3da6d9f4019fa9dd2fcb6ff3f11d5d578bb677635da8089c11dd92e412e65eb00ece45698b169b5e8a502076fd3a312ef4b5a31b76f998fe9e6e8b4ff'
