9a0b7ca400bd7041f4f6ba9e01a249b6feae1ab5
Tair Assimov <tair@assimov.net>
2012-02-10 14:18:02 +0200

    Refactor book unit test to use setup block

--- test/unit/book_test.rb
+++ test/unit/book_test.rb
@@ -1,40 +1,38 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
+  
+  setup do
+    @book = books(:steppenwolf)
+  end
 
   test "a book with all attributes should be valid" do
-    @book = books(:steppenwolf)
     assert @book.valid?
   end
   
   test "a book without a title should not be valid" do
-    @book = books(:steppenwolf)
     @book.title = ''
     assert @book.invalid?
   end
   
   test "a book without authors should not be valid" do
-    book = books(:steppenwolf)
-    book.authors = ""
-    assert book.invalid?
+    @book.authors = ""
+    assert @book.invalid?
   end
 
   test "book title must be unique" do
-    book = books(:steppenwolf)
-    other_book = book.dup
+    other_book = @book.dup
     assert other_book.invalid?
   end
   
   test "a book without ISBN should not be valid" do
-    book = books(:steppenwolf)
-    book.isbn = ""
-    assert book.invalid?
+    @book.isbn = ""
+    assert @book.invalid?
   end
   
   test "a book with an invalid ISBN should not be valid" do
-    book = books(:steppenwolf)
-    book.isbn = "dfjlaksfjds"
-    assert book.invalid?
+    @book.isbn = "dfjlaksfjds"
+    assert @book.invalid?
   end
 
 end
