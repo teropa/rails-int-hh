da89b28d5f42f3edb6329f32a1a1895400f2dc3b
Tair Assimov <tair@assimov.net>
2012-03-02 13:53:40 +0200

    Exercise1: Styling HTML components with Bootstrap: * Change ul to dl in books#index * Add classes to buttons * Style the new/edit book form * Style error messages * Style flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def flash_class(name)
+    if name == :notice
+      "alert alert-success"
+    elsif name == :error
+      "alert alert-error"
+    else
+      "alert"
+    end
+  end
+  
 end
