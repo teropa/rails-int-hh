f53108119157f1226f7697064f8696f9f154b687
Tair Assimov <tair@assimov.net>
2012-03-02 14:27:21 +0200

    Exercise2: Ajaxifying reservation create/free: * Make reservation form remote * Extract reservation status and links to partials * Add JavaScript responder to reservations#create * Implement reservations#create.js.erb to update html parts with Ajax

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,7 +14,10 @@ class ReservationsController < ApplicationController
     @reservation = @book.reservations.new(params[:reservation])
     if @reservation.save
       flash[:notice] = "Book reserved"
-      redirect_to book_path(@book)
+      respond_to do |format|
+        format.html { redirect_to book_path(@book) }
+        format.js
+      end
     else
       render :new
     end
